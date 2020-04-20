496a278dba1f576f24bb27347482659da782173f
Lovee93 <lovee.jain93@gmail.com>
2020-04-20 16:45:52 +1000

    Updates

---
+++ precache-manifest.f62607792946b256ccddf4c43a93692f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7174d56b69d37bf93df8903170ee303",
+    "url": "/hey_watson/index.html"
+  },
+  {
+    "revision": "f6e0dd66bcffc5617180",
+    "url": "/hey_watson/static/css/main.999cf7b7.chunk.css"
+  },
+  {
+    "revision": "0eb04f49eaac4e1f0128",
+    "url": "/hey_watson/static/js/2.1a4e66ea.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/hey_watson/static/js/2.1a4e66ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6e0dd66bcffc5617180",
+    "url": "/hey_watson/static/js/main.4048109a.chunk.js"
+  },
+  {
+    "revision": "85d63fc6d188b9707a1d",
+    "url": "/hey_watson/static/js/runtime-main.0b68c51c.js"
+  }
+]);
