f7c4e4bfeba4004d3695314956f1d9e2d371d8f4
Lovee93 <lovee.jain93@gmail.com>
2020-04-20 16:42:35 +1000

    Initial commit with my project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import recognizeMic from 'watson-speech/speech-to-text/recognize-microphone';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  
+  constructor() {
+    super();
+    this.state = {
+      text: ''
+    }
+  }
+
+  onListenClick(){
+    fetch('https://nameless-fortress-57383.herokuapp.com/api/speech-to-text/token')
+      .then(function(response) {
+        return response.json();
+      }).then((token) => {
+
+    var stream = recognizeMic(Object.assign(token, {
+        objectMode: true, // send objects instead of text
+        format: false // optional - performs basic formatting on the results such as capitals an periods
+    }));
+    
+    stream.on('data', (data) => {
+      this.setState({
+        text: data.results[0].alternatives[0].transcript
+      })
+    });
+
+    stream.on('error', function(err) {
+        console.log(err);
+    });
+
+    document.querySelector('#stop').onclick = stream.stop.bind(stream);
+
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Wanna Speak?</h1>
+          <button onClick={this.onListenClick.bind(this)} className="goForItButton">Go for it!</button>
+          <h3>{this.state.text}</h3>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
